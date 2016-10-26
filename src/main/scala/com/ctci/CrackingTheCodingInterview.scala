93e137c61d6f34df0a8540d2f4fcb1a6dd47cef9
Ignacio Mosca <ignacio.mosca@customink.com>
2016-10-26 14:37:38 -0300

    Add faster version

--- src/main/scala/com/ctci/CrackingTheCodingInterview.scala
+++ src/main/scala/com/ctci/CrackingTheCodingInterview.scala
@@ -12,4 +12,8 @@ object CrackingTheCodingInterview {
     case 0 => a
     case _ => rotate(i - 1, a.tail :+ a.head)
   }
+
+  //faster version
+  def rotate2(k: Int, array: Array[Int]):Array[Int]= array.drop(k) ++ array.take(k)
+
 }
